e6a3bf2952d8018f93f457d099b05f73947ac08d
KimWooYoung <kwy120@naver.com>
2020-01-18 23:15:40 +0900

    setting adapter

--- app/src/main/java/com/kwy/howlstagram/navigation/UserFragment.kt
+++ app/src/main/java/com/kwy/howlstagram/navigation/UserFragment.kt
@@ -5,15 +5,55 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.kwy.howlstagram.R
+import com.kwy.howlstagram.navigation.model.ContentDTO
+import kotlinx.android.synthetic.main.fragment_user.view.*
 
 class UserFragment : Fragment() {
+    var fragmentView: View? = null
+    var firestore: FirebaseFirestore? = null
+    var uid: String? = null
+    var auth: FirebaseAuth? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_user, container, false)
-        return view
+        fragmentView =
+            LayoutInflater.from(activity).inflate(R.layout.fragment_user, container, false)
+        return fragmentView
+    }
+
+    inner class UserFragmentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var contentDTOs: ArrayList<ContentDTO> = arrayListOf()
+
+        init {
+            firestore?.collection("images")?.whereEqualTo("uid", uid)
+                ?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                    // Sometimes, This code return null of querySnapshot when it sign out
+                    if (querySnapshot == null) return@addSnapshotListener
+
+                    // Get data
+                    for (snapshot in querySnapshot.documents) {
+                        contentDTOs.add(snapshot.toObject(ContentDTO::class.java)!!)
+                    }
+                }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun getItemCount(): Int {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
     }
 }
